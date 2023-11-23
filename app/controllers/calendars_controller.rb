ab4fa074a67fd91a82eca91161c26a7958459613
yousuketoyota <147295357+yousuketoyota@users.noreply.github.com>
2023-11-23 20:22:11 +0900

    Revert "Revert "Merge pull request #8 from yousuketoyota/second_training""
    This reverts commit 6b5135f4e5198e8fb87a8cae5cf49fd87aa8aa18.

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -8,14 +8,13 @@ class CalendarsController < ApplicationController
 
   # 予定の保存
   def create
-    Plan.create(plan_params)
+    Plan.create(plan_params)# １プライベートメソッド呼び出し
     redirect_to action: :index
   end
 
   private
-
   def plan_params
-    params.require(:calendars).permit(:date, :plan)
+    params.require(:plan).permit(:date, :plan)# ２特定のキーを受け取る
   end
 
   def get_week
@@ -34,7 +33,16 @@ class CalendarsController < ApplicationController
       plans.each do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans}
+
+
+      wday_num = Date.today.wday + x # wdayメソッドを用いて取得した数値
+      if wday_num >= 7 #「wday_numが7以上の場合」という条件式
+        wday_num = wday_num -7
+      end
+
+      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans, :wday => wdays[wday_num] }
+
+
       @week_days.push(days)
     end
 
